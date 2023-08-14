de25b50920ff1d5cb7bf44bbfb64b00aa9a69cfe
ovarz <ovarz_03@yahoo.com>
2023-08-14 14:50:58 +0700

    Update base.php

--- inc/base.php
+++ inc/base.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="id">
-<base href="/" />
+<base href="/lexus/" />
 <?php 
-  $_SERVER['LEXUS'] = $_SERVER['DOCUMENT_ROOT'] . '/'; 
+  $_SERVER['LEXUS'] = $_SERVER['DOCUMENT_ROOT'] . '/lexus/'; 
   $anticache = date ('s'.'i'.'H'.'d'.'m'.'Y');
 ?>
