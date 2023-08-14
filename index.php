7b17fa5229cf685757d0746da739ef8df7ff4fa3
ovarz <ovarz_03@yahoo.com>
2023-08-14 23:47:03 +0700

    a

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <?php require ($_SERVER['LEXUS'].'inc/meta.php')?>
 <?php require ($_SERVER['LEXUS'].'inc/sample.php')?>
 <?php require ($_SERVER['LEXUS'].'inc/header.php')?>
+<style><?php require ($_SERVER['LEXUS'].'css/home.css')?></style>
 <div class="rancak-container">
 
 
@@ -19,7 +20,6 @@
 	    See All
 	  </a>
 	</div>
-	<style><?php require ($_SERVER['LEXUS'].'css/home.css')?></style>
 	<div class="lexus-product-container">
       <?php 
         $product_array = array();
@@ -36,6 +36,27 @@
           <div class="lpb-car flex_ori thumb-loading">
             <img alt="img_title" class="lazyload" data-original="img/car/car-<?php echo($product_list['product_image'])?>.png" />
           </div>
+        </a>
+	  <?php } ?>
+	</div>
+  </section>
+
+
+
+  <section class="section-container home-menu">
+	<div class="content-container home-menu-container">
+      <?php 
+        $homemenu_array = array();
+        $homemenu_array[]=array('product_title'=>'Finance Calculator','product_icon'=>'1','product_desc'=>'Personalized your own Lexus');
+        $homemenu_array[]=array('product_title'=>'Mobile Stockboard','product_icon'=>'2','product_desc'=>'Monitor Unit Availability');
+        $homemenu_array[]=array('product_title'=>'Learning Material','product_icon'=>'3','product_desc'=>'To Lexus Inspire');
+        $homemenu_array[]=array('product_title'=>'LMS','product_icon'=>'4','product_desc'=>'Manage Your Leads');
+        foreach($homemenu_array as $homemenu_list){
+      ?>
+        <a aria-label="Link_Title" title="Link_Title" class="home-menu-box" href="">
+          <div class="hmb-title"><?php echo($homemenu_list['product_title'])?></div>
+          <div class="hmb-icon content_center"><?php require ($_SERVER['LEXUS'].'img/icon/homemenu-'.$homemenu_list['product_icon'].'.svg')?></div>
+          <div class="hmb-desc"><?php echo($homemenu_list['product_desc'])?></div>
         </a>
 	  <?php } ?>
 	</div>
